44df7f0df577c504af35b7ea4c85f3ac406f3965
WarunaDisanayaka <86902580+WarunaDisanayaka@users.noreply.github.com>
2022-08-20 13:56:08 +0530

    Register

--- Hotel Mangement System/Hotel Mangement System/Customer.cs
+++ Hotel Mangement System/Hotel Mangement System/Customer.cs
@@ -1,48 +1,46 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data;
-using MySql.Data.MySqlClient;
+using System.Windows.Forms;
 
 namespace Hotel_Mangement_System
 {
-    class Customer
+    
+    class Customer : DBconnection
     {
-        public string Id { get; set; }
+
         public string Name { get; set; }
         public string Address { get; set; }
 
-        public int Nic { get; set; }
+        public string Nic { get; set; }
 
-        public int Phone { get; set; }
+        public string Phone { get; set; }
 
-        DBconnection db = new DBconnection();
 
-        
-
-        //update
-
-        public void update(string Name, string Address, string Nic, string Phone)
+        public void register(string Name,string Address,string Nic,string Phone)
         {
+            DBconnection db = new DBconnection();
             db.Connect();
-            using (MySqlCommand cmd = new MySqlCommand())
+            
+            string sql = "INSERT INTO customer(cname,caddress,ctel,cnic)VALUES('"+Name+"','"+Address+"','"+Nic+"','"+Phone+"')";
+          
+            MySqlCommand cm = new MySqlCommand(sql,conn);
+            conn.Open();
+            if (cm.ExecuteNonQuery()==1)
             {
-                cmd.CommandText = "UPDATE customer SET `cname`=@name,`caddress`=@address,`ctel`=@tel WHERE `cnic`=@nic";
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = db.conn;
 
-                cmd.Parameters.Add("@name", MySqlDbType.VarChar).Value = Name;
-                cmd.Parameters.Add("@address", MySqlDbType.VarChar).Value = Address;
-                cmd.Parameters.Add("@tel", MySqlDbType.VarChar).Value = Phone;
-                cmd.Parameters.Add("@nic", MySqlDbType.VarChar).Value = Nic;
-                cmd.Parameters.Add("@id", MySqlDbType.VarChar).Value = Id;
-
-                cmd.ExecuteNonQuery();
-                db.conn.Close();
             }
+            else
+            {
+                MessageBox.Show("Data not instered");
+            }
+
+           
+
         }
 
     }
